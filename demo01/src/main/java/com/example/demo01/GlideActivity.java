4639368d55886a8519feb3df21a0f191a353f753
JerryAlexLiang <1935990239@qq.com>
2018-05-07 10:04:29 +0800

    实现图片高斯模糊(1、RenderScript 2、Glide)

--- demo01/src/main/java/com/example/demo01/GlideActivity.java
+++ demo01/src/main/java/com/example/demo01/GlideActivity.java
@@ -12,7 +12,17 @@ import jp.wasabeef.glide.transformations.BlurTransformation;
 /**
  * 创建日期：2018/5/6 on 下午9:50
  * 描述: 关于Glide的拓展(高斯模糊、加载监听、圆角图片)
- * http://pic4.nipic.com/20090811/2608592_115151007_2.jpg
+ * Glide是一个比较强大也是比较常用的一个图片加载库，Glide中的Transformations用于在图片显示前对图片进行处理。
+ * glide-transformations 这个库为Glide提供了多种多样的 Transformations实现，
+ * 其中就包括高斯模糊的实现BlurTransformation
+ * 步骤：
+ * 1、添加依赖：
+ * compile 'com.github.bumptech.glide:glide:3.7.0'
+ * compile 'jp.wasabeef:glide-transformations:2.0.1'
+ * 2、通过这两个库的结合使用，就可以使用其中的BlurTransformation实现图片的高斯模糊
+ * 其中radius的取值范围是1-25，radius越大，模糊度越高。
+ * Glide.with(context).load(R.drawable.defalut_photo)
+ * .bitmapTransform(new BlurTransformation(context, radius)).into(mImageView);
  * 作者: liangyang
  */
 public class GlideActivity extends AppCompatActivity {
--- demo01/src/main/java/com/example/demo01/utils/BitmapUtils.java
+++ demo01/src/main/java/com/example/demo01/utils/BitmapUtils.java
@@ -23,6 +23,16 @@ import java.net.URLConnection;
 public class BitmapUtils {
     /**
      * 对Bitmap进行高斯模糊
+     * RenderScript是Google在Android 3.0(API 11)中引入的一个高性能图片处理框架。
+     * 使用RenderScriprt实现高斯模糊:
+     * 首先在在build.gradle的defaultConfig中添加RenderScript的使用配置
+     * renderscriptTargetApi 24
+     * renderscriptSupportModeEnabled true
+     * renderscriptTargetApi :
+     * 指定要生成的字节码版本。我们(Goole官方)建议您将此值设置为最低API级别能够提供所有的功能，你使用和设置renderscriptSupportModeEnabled为true。此设置的有效值是从11到
+     * 最近发布的API级别的任何整数值。
+     * renderscriptSupportModeEnabled:
+     * 指定生成的字节码应该回落到一个兼容的版本，如果运行的设备不支持目标版本。
      *
      * @param bitmap  图片资源
      * @param context 上下文
@@ -32,32 +42,42 @@ public class BitmapUtils {
     public static Bitmap blurBitmap(Bitmap bitmap, Context context) {
 
         //Let's create an empty bitmap with the same size of the bitmap we want to blur
+        //用需要创建高斯模糊bitmap创建一个空的bitmap
         Bitmap outBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
 
         //Instantiate a new Renderscript
+        // 初始化Renderscript，该类提供了RenderScript context，创建其他RS类之前必须先创建这个类，
+        // 其控制RenderScript的初始化，资源管理及释放
         RenderScript rs = RenderScript.create(context);
 
         //Create an Intrinsic Blur Script using the Renderscript
+        // 创建高斯模糊对象
         ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
 
         //Create the Allocations (in/out) with the Renderscript and the in/out bitmaps
+        // 创建Allocations，此类是将数据传递给RenderScript内核的主要方 法，并制定一个后备类型存储给定类型
         Allocation allIn = Allocation.createFromBitmap(rs, bitmap);
         Allocation allOut = Allocation.createFromBitmap(rs, outBitmap);
 
         //Set the radius of the blur: 0 < radius <= 25,设置模糊度(在0.0到25.0之间)，默认”25"
+        //设定模糊度(注：Radius最大只能设置25.f)
         blurScript.setRadius(12.0f);//这个是设置模糊度，取值在0 < radius <= 25，值越大越模糊
 
         //Perform the Renderscript
+        //执行渲染脚本
         blurScript.setInput(allIn);
         blurScript.forEach(allOut);
 
         //Copy the final bitmap created by the out Allocation to the outBitmap
+        //复制由out分配到outBitmap的最终位图。
         allOut.copyTo(outBitmap);
 
         //recycle the original bitmap
+        //回收原始位图
         bitmap.recycle();
 
         //After finishing everything, we destroy the Renderscript.
+        //fter完成了所有任务，我们销毁了Renderscript。
         rs.destroy();
 
         return outBitmap;
@@ -66,7 +86,8 @@ public class BitmapUtils {
     /**
      * 网络图片，用url生成Bitmap的方法
      * 根据图片url获取图片对象
-     *这个方法不能在UI线程中执行,用Thread+Handler实现
+     * 这个方法不能在UI线程中执行,用Thread+Handler实现
+     *
      * @param urlpath
      * @return
      */
